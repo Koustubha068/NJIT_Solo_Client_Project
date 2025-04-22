bbfe5fdfe54b3e26f7b6ebb89ebc82782adea19b
Koustubha068 <KoustuK@gmail.com>
2025-04-22 14:55:16 -0400

    almost done with navbar

--- script.js
+++
@@ -1,8 +0,0 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='flex'
-}
-function hideSiderbar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='none'
-}
