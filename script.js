08d1bf3b5b0582d30ba4c82f577344a14eaefb0c
Koustubha068 <KoustuK@gmail.com>
2025-05-29 23:11:06 -0400

    working on the hero section & navbar

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='flex'
-}
-function hideSiderbar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='none'
-}
+const container = document.querySelector('.container');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
