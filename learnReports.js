610f1a1764e070cfb1cd098460176fe9645efc4d
Koustubha068 <KoustuK@gmail.com>
2025-06-03 16:26:12 -0400

    almost done. Just need to change colors on LearnReports

---
+++ learnReports.js
@@ -0,0 +1,95 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".nav-links-container");
+
+// Toggle the navigation visibility on hamburger click
+hamburger.addEventListener("click", function () {
+  navLinksContainer.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+
+
+
+
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-green-500");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-red-500");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
+    });
+});
+
+// about section
+
+// Function to check if an element is in the viewport
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Select the image-text section
+const imageTextSection = document.querySelector('.image-text-section');
+
+// Listen for scroll events
+window.addEventListener('scroll', function() {
+  if (isInViewport(imageTextSection)) {
+    imageTextSection.classList.add('visible');
+  }
+});
+
+
+//FAQ section
+
+document.addEventListener('DOMContentLoaded', function() {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    question.addEventListener('click', () => {
+      item.classList.toggle('open');
+    });
+  });
+});
