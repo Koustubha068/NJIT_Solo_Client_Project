79edcbbdc39f7d621f81ceff1d557b120760f5cc
Koustubha068 <KoustuK@gmail.com>
2025-06-03 15:28:30 -0400

    almost done with FAQ section. Just need to style

--- getCreatedReports.js
+++ getCreatedReports.js
@@ -81,4 +81,15 @@ window.addEventListener('scroll', function() {
 });
 
 
-//Tools section
+//FAQ section
+
+document.addEventListener('DOMContentLoaded', function() {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    question.addEventListener('click', () => {
+      item.classList.toggle('open');
+    });
+  });
+});
